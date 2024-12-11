79170ce81ba050a660734e4a38e870dc54f2c1ed
RogersCodes <adindajohnrogers@gmail.com>
2024-12-12 01:42:07 +0300

    feat: added the slice method

--- races.js
+++ races.js
@@ -2,7 +2,12 @@ const races = ["👨", "👨🏻", "👨🏼", "👨🏽", "👨🏿"];
 
 const sortByRace = (a, b) => {
     const raceOrder = { "👨": 1, "👨🏻": 2, "👨🏼": 3, "👨🏽": 4, "👨🏿": 5 };
-    const skinTone = races.slice(-1);
-    return raceOrder[skinTone] || 0;
+    const getSkinTone = (races) => {
+        const skinTone = races.slice(-1);
+        return raceOrder[skinTone] || 0;
+    };
+
+    return getSkinTone(b) - getSkinTone(a);
 };
-console.log("Sorted Races:", sortedEmojis);
+const sortedRaces = races.sort(sortByRace);
+console.log("Sorted Races:", sortedRaces);
