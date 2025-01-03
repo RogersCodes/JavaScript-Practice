d90c06a55f08685e55103441a825f637b6d2124e
RogersCodes <adindajohnrogers@gmail.com>
2025-01-03 21:35:29 +0300

    feat: Leetcode memoize

---
+++ practice/memoize.js
@@ -0,0 +1,47 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+
+function memoize(fn) {
+    const cache = new Map();
+
+    return function (...args) {
+        const key = JSON.stringify(args);
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+       
+        const result = fn(...args);
+        cache.set(key, result);
+        return result;
+    };
+}
+
+const sum = (a, b) => a + b;
+const fib = (n) => (n <= 1 ? 1 : fib(n - 1) + fib(n - 2));
+
+const factorial = (n) => (n <= 1 ? 1 : factorial(n - 1) * n);
+const memoizedSum = memoize(sum);
+const memoizedFib = memoize(fib);
+const memoizedFactorial = memoize(factorial);
+
+let callCount = 0;
+const memoizedFn = memoize(function (a, b) {
+    callCount += 1;
+    return a + b;
+});
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
